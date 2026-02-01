8127cc721f461e4aa6c91b7f63459882cd8ab185
Md Saquib <mdsaquib5540@gmail.com>
2026-02-02 03:12:50 +0530

    okay

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
 import connectDB from "./config/db.js";
@@ -7,10 +8,6 @@ import connectDB from "./config/db.js";
 const PORT = process.env.PORT || 4000;
 
 connectDB();
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("Mongo error:", err));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
