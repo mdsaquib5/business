f8dc3435e96303568d915453b01343e4c05284ae
Md Saquib <mdsaquib5540@gmail.com>
2026-02-02 03:01:18 +0530

    okay

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 4000;
 
 connectDB();
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Mongo error:", err));
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
